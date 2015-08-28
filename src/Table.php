309d70547403f9072d33f48607f35b010fde5d84
Joe Green <joe.green@flashtalking.com>
2015-08-28 12:05:36 +0100

    minor cache optimisation

--- src/Table.php
+++ src/Table.php
@@ -197,7 +197,7 @@ class Table {
             if(count($startIndex) > 1)
             {
                 // Optimisation to isolate first item in index - also works well for partition pruning
-                $query->where($this->cachePrimaryKey[0], '>=', reset($startIndex));
+                //$query->where($this->cachePrimaryKey[0], '>=', reset($startIndex));
             }
         }
 
@@ -208,7 +208,7 @@ class Table {
             if(count($endIndex) > 1)
             {
                 // Optimisation to isolate first item in index - also works well for partition pruning
-                $query->where($this->cachePrimaryKey[0], '<=', reset($endIndex));
+                //$query->where($this->cachePrimaryKey[0], '<=', reset($endIndex));
             }
         }
     }
