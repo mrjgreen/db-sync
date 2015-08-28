a3a91c49de85263a7762215a93c4d6af8edc4a48
Joe Green <joe.green@flashtalking.com>
2015-08-28 14:52:52 +0100

    minor cache optimisation

--- src/Table.php
+++ src/Table.php
@@ -217,12 +217,12 @@ class Table {
         {
             $query->whereRaw($this->getWhereEnd(), $endIndex);
 
-            // If we have start index this will already have been covered above
-            if(!$startIndex && $compoundPrimary)
-            {
-                // Optimisation to isolate first item in index - also works well for partition pruning
-                $query->where($this->cachePrimaryKey[0], '<=', reset($endIndex));
-            }
+//            // If we have start index this will already have been covered above
+//            if(!$startIndex && $compoundPrimary)
+//            {
+//                // Optimisation to isolate first item in index - also works well for partition pruning
+//                $query->where($this->cachePrimaryKey[0], '<=', reset($endIndex));
+//            }
         }
     }
 
