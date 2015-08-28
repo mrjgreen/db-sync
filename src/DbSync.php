a57911dfe9f6a77b70cec701c98b8a0c4cf5b439
Joe Green <joe.green@flashtalking.com>
2015-08-28 12:23:52 +0100

    minor cache optimisation

--- src/DbSync.php
+++ src/DbSync.php
@@ -131,6 +131,13 @@ class DbSync {
                 $this->logger->debug("Reached end of results set table '$source' at block '$i' at block size '$blockSize'");
                 break;
             }
+
+            if($blockSize == $this->blockSize)
+            {
+                $checked = $i * $blockSize;
+
+                $this->logger->info("Written '$rowCount' rows, checked '$checked' rows for tables '$source' => '$destination'");
+            }
         }
 
         return $rowCount;
