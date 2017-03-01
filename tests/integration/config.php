8b3c50e56b27c141352bb96380b192c46abf48a1
Joe Green <joe.green0991@gmail.com>
2017-03-01 23:04:28 +0000

    fixed config error for tests

--- tests/integration/config.php
+++ tests/integration/config.php
@@ -18,17 +18,3 @@ return [
         'collation' => 'utf8_unicode_ci',
     ]
 ];
-
-
-,
-"require": {
-  "psr/log": "*",
-  "symfony/console": "^2.7",
-  "mrjgreen/database": "^2.4",
-  "symfony/debug": "^2.7",
-  "ramsey/array_column": "^1.1"
-},
-"require-dev": {
-  "satooshi/php-coveralls": "dev-master",
-  "phpunit/phpunit": "^4.8"
-}
